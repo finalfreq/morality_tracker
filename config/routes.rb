e7cd7088373cfcceb5a7ed08421950203a6ea094
james williams & brent bailey <student@epicodus.com>
2015-07-15 10:21:09 -0700

    add views for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :actions, except: :show
+  resources :items, except: :show
+  resources :users, only: [:show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/migrate/20150715170342_change_join_table_names.rb
+++ db/migrate/20150715170342_change_join_table_names.rb
@@ -2,5 +2,6 @@ class ChangeJoinTableNames < ActiveRecord::Migration
   def change
     rename_table :users_actions, :actions_users
     rename_table :users_items, :items_users
+
   end
 end
